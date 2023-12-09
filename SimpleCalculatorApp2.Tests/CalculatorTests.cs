7708362ba3cf40a574912721cfa411fb39e1d149
JGMelon22 <jgvasconcellos22@gmail.com>
2023-12-09 16:15:57 -0300

    first commit

---
+++ SimpleCalculatorApp2.Tests/CalculatorTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace SimpleCalculatorApp2.Tests
+{
+    public class CalculatorTests
+    {
+        [Theory]
+        [InlineData(5, 3, 8)]
+        public void Calculator_Add_ReturnInt(int a, int b, int expected)
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var result = calculator.Add(a, b);
+
+            // Assert
+            result.Should().Be(expected);
+            result.Should().BeGreaterOrEqualTo(8);
+            result.Should().NotBe(null);
+        }
+
+        [Theory]
+        [InlineData(10, 2, 8)]
+        [InlineData(30, 200, -170)]
+        public void Calculator_Subtract_ReturnInt(int a, int b, int expected)
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var result = calculator.Subtract(a, b);
+
+            // Assert
+            result.Should().Be(expected);
+            result.Should().BeLessOrEqualTo(8);
+            result.Should().NotBe(null);
+        }
+
+        [Theory]
+        [InlineData(4, 5, 20)]
+        public void Calculator_Multiply_ReturnInt(int a, int b, int expected)
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var result = calculator.Multiply(a, b);
+
+            // Assert
+            result.Should().Be(expected);
+            result.Should().BeLessOrEqualTo(20);
+            result.Should().NotBe(null);
+        }
+
+        [Theory]
+        [InlineData(10, 2, 5)]
+        [InlineData(5, 1, 5)]
+        public void Calculator_Divide_ReturnInt(int a, int b, int expected)
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var result = calculator.Divide(a, b);
+
+            // Assert
+            result.Should().Be(expected);
+            result.Should().BeGreaterOrEqualTo(5);
+            result.Should().NotBe(null);
+        }
+
+        [Theory]
+        [InlineData(2, 0)]
+        public void Calculator_Divide_ReturnDivideByZeroException(int a, int b)
+        {
+            // Arrange 
+            var calculator = new Calculator();
+
+            // Act + Assert
+            Action action = () => calculator.Divide(a, b);
+            action.Should().NotBeNull();
+            action.Should().Throw<DivideByZeroException>();
+        }
+    }
+}
---
+++ SimpleCalculatorApp2.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SimpleCalculatorApp2.CalculatorTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleCalculatorApp2.CalculatorTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("16815106-01A0-4196-B9AE-6666FBC277EE")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
